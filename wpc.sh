593256ad83d835448b21d2441fdbab0198990a31
Mr. Sorensen <mrsorensen@protonmail.com>
2020-03-18 13:26:53 +0100

    Redesigned the whole script

--- wpc.sh
+++ wpc.sh
@@ -1,115 +1,45 @@
 #!/bin/bash
 
-#	CONFIG
-# ---------------------------
-
-# Set either nitrogen or feh to 1
-# to select which dependency you
-# want to use
-nitrogen=0
-feh=0
-wal=1
-
-# At which hour wallpaper should change
-tMorning=06
-tDay=10
-tAfternoon=20
-tNight=22
-
-# Dir to images you want to use
-dir=~/Pictures/Firewatch/
-
-# Images in dir you want to use
-iMorning=morning.png
-iDay=day.png
-iAfternoon=afternoon.png
-iNight=night.png
+# Set directory to first arguement
+dir=$1
 
 
-# Monitor -- use monitor0 if default does work (Default: monitorLVDS1)
-monitor=monitorLVDS1
+# changed=0
 
-
-
-
-#	END OF CONFIG
-# ---------------------------
-
-
-
-echo "---------------"
-
-# Keep looping every 10 minutes
+# Loop
 while (true)
 do
 	# Get current hour in 24h format
-	curh=$(date +"%-H")
+	currentHour=$(date +"%-H")
 
-	# Prepare image to be set as wallpaper depending on time
-	if (( "$curh" >= "$tMorning" && "$curh" < "$tDay" )) ; then
-		if [[ "$img" == $iMorning ]] ; then
-			changed=1
-		else
-			changed=0
-			img=$iMorning
+	# Reversed loop on all items in dir
+	for t in $(ls -r $dir)
+	do
+		# Set base filename
+		basefile=$(basename $t .png)
+		# Check if current hour is higher than base filename
+		if (( "$currentHour" >= "10#$basefile" )) ; then
+			# Check if basefile image already is set
+			if [[ "$currentImg" == "$basefile" ]] ; then
+				changed=1
+			else
+				changed=0
+				currentImg=$basefile
+			fi
+			# Change wallpaper if not set
+			if [[ "$changed" == 0 ]] ; then
+				changed=1
+				echo "changing to $basefile.png"
+				wal -a 70 -i $dir/$basefile.png
+				break
+			else
+				break
+			fi
 		fi
-	elif (( "$curh" >= "$tDay" && "$curh" < "$tAfternoon" )) ; then
-		if [[ "$img" == $iDay ]] ; then
-			changed=1
-		else
-			changed=0
-			img=$iDay
-		fi
-	elif (( "$curh" >= "$tAfternoon" && "$curh" < "$tNight" )) ; then
-		if [[ "$img" == $iAfternoon ]] ; then
-			changed=1
-		else
-			changed=0
-			img=$iAfternoon
-		fi
-	elif (( "$curh" >= "$tNight" || "$curh" < "$tMorning" )) ; then
-		if [[ "$img" == $iNight ]] ; then
-			changed=1
-		else
-			changed=0
-			img=$iNight
-		fi
-	fi
+	done
 
 
-	# Concatenate the image name on the directory it is in
-	imgPath="$dir$img"
 
-
-	# Change wallpaper if it's not already set 
-	if [[ "$changed" == 0 ]] ; then
-		changed=0
-		if [[ "$nitrogen" == 1 ]] ; then
-			nitrogen --set-scaled $imgPath
-		elif [[ "$wal" == 1 ]] ; then
-			wal -i $imgPath
-		elif [[ "$feh" == 1 ]] ; then
-			feh --bg-scale $imgPath
-		fi
-
-		# Clear screen
-		clear
-
-		# Print info
-		if [[ "$img" ==  "$iMorning" ]] ; then
-			echo "Morning"
-		elif [ "$img" =  "$iDay" ] ; then
-			echo "Day"
-		elif [ "$img" =  "$iAfternoon" ] ; then
-			echo "Evening"
-		elif [ "$img" =  "$iNight" ] ; then
-			echo "Night"
-		fi
-
-	fi
-
-
-	# Seconds to sleep before checking the time again -- default 600 (10 minutes)
-	sleep 600
-
+	# Sleep before repeating loop
+	sleep 60
 done
